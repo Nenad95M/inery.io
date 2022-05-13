0857e3f20631fd28624b86383a36f525edbe7e7d
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-05-13 16:50:12 +0200

    show more users

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,89 +1,86 @@
 //filtriranje poslova po kategorijama
-const jobFilter= function(){
-const allJobsBtn = document.getElementById("all-jobs");
-const blockchainJobs = document.getElementById("blockchain-jobs");
-const frontEndJobs = document.getElementById("front-end-jobs");
-const backEndJobs = document.getElementById("back-end-jobs");
-const filterButtons=Array.from(document.querySelector('.job-filter').children);
-console.log(filterButtons);
-let jobsContainer = document
-  .getElementById("jobs-container")
-  .getElementsByClassName("job-card");
+const jobFilter = function () {
+  const allJobsBtn = document.getElementById("all-jobs");
+  const blockchainJobs = document.getElementById("blockchain-jobs");
+  const frontEndJobs = document.getElementById("front-end-jobs");
+  const backEndJobs = document.getElementById("back-end-jobs");
+  const filterButtons = Array.from(
+    document.querySelector(".job-filter").children
+  );
+  let jobsContainer = document
+    .getElementById("jobs-container")
+    .getElementsByClassName("job-card");
 
-jobsContainer = Array.from(jobsContainer);
+  jobsContainer = Array.from(jobsContainer);
 
-
-function removeActive(){
-  filterButtons.forEach(element => {
-  if(element.classList.contains('active')){
-    element.classList.remove('active');
-  }
-  });
-}
-function addActive(e){
-  e.target.classList.add('active');
-console.log(e.target.classList);
-}
-
-function displayAll() {
-  jobsContainer.forEach((element) => {
-    element.style.display = "flex";
-  });
-}
-function takeCareOfDOM(e){
-  removeActive();
-  addActive(e);
-  displayAll();
-}
-function jobCategoryFilter(jobCategory){
-  jobsContainer.forEach((element) => {
-    if (element.getAttribute("data-category") !== jobCategory) {
-      element.style.display = "none";
+  function takeCareOfDOM(e) {
+    function addActive(e) {
+      e.target.classList.add("active");
+      console.log(e.target.classList);
     }
-  });
-}
-allJobsBtn.addEventListener('click',function(e){
-  takeCareOfDOM(e);
-})
-
-blockchainJobs.addEventListener("click", function (e) {
-  takeCareOfDOM(e);
-  let jobCategory="blockchain";
-  jobCategoryFilter(jobCategory);
-
-});
-
-frontEndJobs.addEventListener("click", function (e) {
-  takeCareOfDOM(e);
-  let jobCategory="front-end";
-  jobCategoryFilter(jobCategory);
-});
-
-backEndJobs.addEventListener("click", function (e) {
-  takeCareOfDOM(e);
-  let jobCategory="back-end";
-  jobCategoryFilter(jobCategory);
-});
-}
-//Simulacija fecovanja dodatnih poslova iz neke baze
-const moreJobs=function(){
-  //da bi se api samo jednom pozvao
- let called=false
-  function callApi() {
-    fetch('/databaseSimulation/data.json').then(response => response.json()).then(data => {
-     if (called==false){
-      makeUI(data['jobs'])
-    jobFilter();
-    called=true;
-     }
+    function displayAll() {
+      jobsContainer.forEach((element) => {
+        element.style.display = "flex";
       });
-    
- }
- const ui=document.getElementById("jobs-container");
+    }
+    function removeActive() {
+      filterButtons.forEach((element) => {
+          element.classList.remove("active");
+      });
+    }
+    removeActive();
+    addActive(e);
+    displayAll();
+  }
+  function jobCategoryFilter(jobCategory) {
+    jobsContainer.forEach((element) => {
+      if (element.getAttribute("data-category") !== jobCategory) {
+        element.style.display = "none";
+      }
+    });
+  }
+  allJobsBtn.addEventListener("click", function (e) {
+    takeCareOfDOM(e);
+  });
 
- function makeUI(data){
-   data.forEach(element => {
-  ui.innerHTML+= `<article class="job-card" data-category="${element.category}">
+  blockchainJobs.addEventListener("click", function (e) {
+    takeCareOfDOM(e);
+    let jobCategory = "blockchain";
+    jobCategoryFilter(jobCategory);
+  });
+
+  frontEndJobs.addEventListener("click", function (e) {
+    takeCareOfDOM(e);
+    let jobCategory = "front-end";
+    jobCategoryFilter(jobCategory);
+  });
+
+  backEndJobs.addEventListener("click", function (e) {
+    takeCareOfDOM(e);
+    let jobCategory = "back-end";
+    jobCategoryFilter(jobCategory);
+  });
+};
+//Simulacija fecovanja dodatnih poslova iz neke baze
+const moreJobs = function () {
+  let called = false;
+  //da bi se api samo jednom pozvao
+  function callApi() {
+    if (called == false) {
+    fetch("/databaseSimulation/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+          makeUI(data["jobs"]);
+          jobFilter();
+          called = true;
+      });
+  }
+}
+  const ui = document.getElementById("jobs-container");
+
+  function makeUI(data) {
+    data.forEach((element) => {
+      ui.innerHTML += `<article class="job-card" data-category="${element.category}">
   <div class="meta-info">
     <div class="category ${element.category}">${element.job}</div>
     <div class="long-time-ago">22h ago</div>
@@ -105,26 +102,59 @@ const moreJobs=function(){
   </div>
   <a class="apply-btn" href="/application.html#application-form">Apply now</a>
 
-</article>`
-});
+</article>`;
+    });
+  }
 
-}
+  document.getElementById("show-all-jobs").addEventListener("click", callApi);
+};
 
-document.getElementById("show-all-jobs").addEventListener('click', callApi)
+//dinamicno ubacujem sadrzaj u html
+const showEvenMoreJobs=function() {
+  let called = false;
+  //da bi se api samo jednom pozvao
+  function callApi() {
+    if (called == false) {
+    fetch("/databaseSimulation/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+          makeUI(data["jobs"]);
+          called = true;
+      });
+  }
 }
-// validacija subscribe inputa
-const subscribeValidation=function(){
- const subscribeForm=document.getElementById('subscribe-form');
- subscribeForm.addEventListener('invalid', function(e){
-   console.log('invalid');
-   e.preventDefault;
- })
-}
+  const ui = document.getElementById("job-table");
+  function makeUI(data) {
+    data.forEach((element) => {
+      ui.innerHTML += `<div class="row">
+      <div class="job-avatar">
+          <div class="js-img"></div>
+          <div class="job-title">
+              <h3>${element.title}</h3>
+              <small>Front-End</small>
+          </div>
+      </div>
+      <div class="location"><img draggable="false" src="/assets/img/similarOffers/location-remote.png"
+              alt="remote location"></div>
+      <div class="level"><img draggable="false" src="/assets/img/similarOffers/level-medior.png"
+              alt="medior level"></div>
+      <div class="department"><img draggable="false"
+              src="/assets/img/similarOffers/department-development.png" alt="development department">
+      </div>
+  </div>`;
+    });
+  }
+
+  document.getElementById("all-jobs").addEventListener("click", callApi);
+
+} 
 
 //proveravam da li html fajl sadrzi elemente sa kojima rade selektori
 //ako sadrzi funkcije se inicijalizuju
-if(document.getElementById("jobs-container")){
-    jobFilter();
-    moreJobs();
+if (document.getElementById("jobs-container")) {
+  jobFilter();
+  moreJobs();
 }
-
+if(document.getElementById("job-table")) {
+  showEvenMoreJobs();
+}
